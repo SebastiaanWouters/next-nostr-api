4fb8aceebcd0dfb9a59714603207927745f4eaed
Sebastiaan <sebastiaan.wouters@student.kuleuven.be>
2023-02-15 01:59:37 +0100

    add api route to fetch event from id

---
+++ pages/api/event/[id].js
@@ -0,0 +1,22 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { pool, relays } from '../../../utils/relay.js'
+import { Author, Event, RelayPool } from 'nostr-relaypool-fork'
+import { asyncCallWithTimeout } from '@/utils/helpers.js';
+
+
+export default function handler(req, res) {
+  const { id } = req.query;
+
+  const getEvent = async (id, relays) => {
+    try {
+        const event = await pool.getEventById(id, relays);
+        res.status(200).json(event);
+    }
+    catch (err) {
+      res.status(400).json(err);
+    }
+  }
+  getEvent(id, relays);
+
+
+}
--- utils/relay.js
+++ utils/relay.js
@@ -1,7 +1,9 @@
 import {RelayPool} from "nostr-relaypool-fork";
 
 export const relays = [
+  "wss://relay.snort.social",
   "wss://nostr-pub.wellorder.net",
+  "wss://brb.io"
 ];
 
 export const pool = new RelayPool(relays);
